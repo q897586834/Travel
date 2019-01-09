498e64683237f6804ccf3785be1d5d74587aa13e
q897586834 <897586834@qq.com>
2019-01-09 18:04:18 +0800

    1/9

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from "./Header";
+export default {
+  name: "HelloWorld",
+  components: {
+    HomeHeader
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
