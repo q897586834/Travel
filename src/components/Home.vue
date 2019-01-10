f4083e0238950f9800e30d665a47037b418bbb2b
q897586834 <897586834@qq.com>
2019-01-10 15:42:19 +0800

    2019/1/10/15:42

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,16 +2,18 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from "./Home/Header";
-import HomeSwiper from './Home/Swiper'
+import HomeSwiper from './Home/Swiper';
+import HomeIcons from './Home/Icons';
 export default {
   name: "HelloWorld",
   components: {
-    HomeHeader,HomeSwiper
+    HomeHeader,HomeIcons,HomeSwiper
   }
 };
 </script>
